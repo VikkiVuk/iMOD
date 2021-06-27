b5233a89f412a47bc7cf0264d138636c00e6f498
VikkiVuk <viktorblazaric2@gmail.com>
2021-06-27 16:24:44 +0200

    a

--- src/main/java/com/VikkiVuk/iMOD/iMOD.java
+++ src/main/java/com/VikkiVuk/iMOD/iMOD.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package com.VikkiVuk.iMOD;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class iMOD implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
